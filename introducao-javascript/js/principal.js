e4718f7a424d7a8a4741d051dd73b5605c464ea6
JoaoPedroCordeiroC <joao.u@hotmail.com>
2019-06-19 17:57:06 -0300

    uso de for para calcular todos os pacientes

--- introducao-javascript/js/principal.js
+++ introducao-javascript/js/principal.js
@@ -1,35 +1,37 @@
 var titulo = document.querySelector('.titulo');
 titulo.textContent = 'Café';
 
-var paciente = document.querySelector('#primeiro-paciente');
+var pacientes = document.querySelectorAll('.paciente');
 
-var tdPeso = paciente.querySelector('.info-peso');
-var peso = tdPeso.textContent;
+for(var i = 0; i < pacientes.length; i++) {
+    var paciente = pacientes[i];
 
-var tdAltura = paciente.querySelector('.info-altura');
-var altura = tdAltura.textContent;
-
-var tdImc = paciente.querySelector(".info-imc");
-
-var pesoValido = true;
-var alturaValido = true;
-
-if(peso <= 0 || peso >= 1000) {
-    console.log('Peso inválido!');
-    pesoValido =  false;
-    tdImc.textContent = 'Peso Inválido!';
+    var tdPeso = paciente.querySelector('.info-peso');
+    var peso = tdPeso.textContent;
+    
+    var tdAltura = paciente.querySelector('.info-altura');
+    var altura = tdAltura.textContent;
+    
+    var tdImc = paciente.querySelector(".info-imc");
+    
+    var pesoValido = true;
+    var alturaValido = true;
+    
+    if(peso <= 0 || peso >= 1000) {
+        console.log('Peso inválido!');
+        pesoValido =  false;
+        tdImc.textContent = 'Peso Inválido!';
+    }
+    
+    if(altura <= 0 || altura >= 3.00) {
+        console.log('Altura inválido!');
+        pesoValido =  false;
+        tdImc.textContent = 'Altura Inválida!';
+    }
+    
+    if (alturaValido && pesoValido) {
+        var imc = peso / (altura * altura);
+        //Setando valor para campo imc no HTML
+        tdImc.textContent = imc.toFixed(2);
+    }
 }
-
-if(altura <= 0 || altura >= 3.00) {
-    console.log('Altura inválido!');
-    pesoValido =  false;
-    tdImc.textContent = 'Altura Inválida!';
-}
-
-if (alturaValido && pesoValido) {
-    var imc = peso / (altura * altura);
-    //Setando valor para campo imc no HTML
-    tdImc.textContent = imc;
-}
-
-console.log(imc);  
