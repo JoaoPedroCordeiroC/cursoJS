2215a6681b7ad782199676a869c3c0a30099ceac
JoaoPedroCordeiroC <joao.u@hotmail.com>
2019-06-21 11:22:49 -0300

    melhorando o código. Criando objeto cliente e funções para criar e inserir linhas e colunas

--- introducao-javascript/js/form.js
+++ introducao-javascript/js/form.js
@@ -3,34 +3,53 @@ botaoAdicionar.addEventListener('click', function(event){   //Função anonima
     event.preventDefault();                                // Previne o evento padrão do botão   
     
     var form = document.querySelector('#form-adiciona');
+    // Extraindo informaçoes do pacinete do fomr
+    var  paciente = obtemPacienteDoFormulario(form);
 
-    var nome = form.nome.value;
-    var peso = form.peso.value;
-    var altura = form.altura.value;
-    var gordura = form.gordura.value;
+    console.log(paciente);
 
-    var pacienteTr = document.createElement('tr');  //Cria linha de tabela
+    //Cria a tr e a td do paciente
+    var pacienteTr = montaTr(paciente);
 
-    var nomeTd = document.createElement('td');      //Cria as colunas da linha
-    var pesoTd = document.createElement('td');      
-    var alturaTd = document.createElement('td');
-    var gorduraTd = document.createElement('td');
-    var imcTd = document.createElement('td');
-
-    nomeTd.textContent = nome;
-    pesoTd.textContent = peso;
-    alturaTd.textContent = altura;
-    gorduraTd.textContent = gordura;
-    imcTd.textContent = calculaImc(peso,altura);
-
-    pacienteTr.appendChild(nomeTd);     //Colocando as colunas dentro da linha 
-    pacienteTr.appendChild(pesoTd);
-    pacienteTr.appendChild(alturaTd);
-    pacienteTr.appendChild(gorduraTd);
-    pacienteTr.appendChild(imcTd);
-    
+    //Colocando a linha dentro da tabela
     var tabela = document.querySelector('#tabela-pacientes');
 
-    //Colocando a liinha dentro da tabela
     tabela.appendChild(pacienteTr);
+    
+    form.reset(); //Limpa os campos
 })
+
+function obtemPacienteDoFormulario(form) {
+
+    var paciente = {
+        nome: form.nome.value,
+        peso: form.peso.value,
+        altura: form.altura.value,
+        gordura: form.gordura.value,
+        imc: calculaImc(form.peso.value, form.altura.value)
+    }
+
+   return paciente;
+}
+
+function montaTr(paciente) {
+
+    var pacienteTr = document.createElement('tr');  //Cria linha de tabela
+    pacienteTr.classList.add('paciente');
+
+    pacienteTr.appendChild( montaTd(paciente.nome, 'info-nome')); 
+    pacienteTr.appendChild( montaTd(paciente.peso, 'info-peso'));
+    pacienteTr.appendChild( montaTd(paciente.altura, 'info-altura'));
+    pacienteTr.appendChild( montaTd(paciente.gordura, 'info-gordura'));
+    pacienteTr.appendChild( montaTd(paciente.imc, 'info-imc'));
+
+    return pacienteTr;
+}
+
+function montaTd(dado,classe) {
+    var td = document.createElement('td');
+    td.textContent = dado;
+    td.classList.add(classe);
+
+    return td;
+}
