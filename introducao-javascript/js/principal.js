513d0b1e5804f0d396496f16e223610105c33d60
JoaoPedroCordeiroC <joao.u@hotmail.com>
2019-06-21 09:41:32 -0300

    criando linha e colunas e inserindo na tabela

--- introducao-javascript/js/principal.js
+++ introducao-javascript/js/principal.js
@@ -50,8 +50,26 @@ botaoAdicionar.addEventListener('click', function(event){   //Função anonima
     var altura = form.altura.value;
     var gordura = form.gordura.value;
 
-    console.log(nome);
-    console.log(peso);
-    console.log(altura);
-    console.log(gordura);
+    var pacienteTr = document.createElement('tr');  //Cria linha de tabela
+
+    var nomeTd = document.createElement('td');      //Cria as colunas da linha
+    var pesoTd = document.createElement('td');      
+    var alturaTd = document.createElement('td');
+    var gorduraTd = document.createElement('td');
+    var imcTd = document.createElement('td');
+
+    nomeTd.textContent = nome;
+    pesoTd.textContent = peso;
+    alturaTd.textContent = altura;
+    gorduraTd.textContent = gordura;
+
+    pacienteTr.appendChild(nomeTd);     //Colocando as colunas dentro da linha 
+    pacienteTr.appendChild(pesoTd);
+    pacienteTr.appendChild(alturaTd);
+    pacienteTr.appendChild(gorduraTd);
+
+    var tabela = document.querySelector('#tabela-pacientes');
+
+    //Colocando a liinha dentro da tabela
+    tabela.appendChild(pacienteTr);
 })
