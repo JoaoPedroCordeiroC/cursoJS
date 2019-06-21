331d7c92967d94a744ea2c527270c27c8b191f75
JoaoPedroCordeiroC <joao.u@hotmail.com>
2019-06-21 16:39:01 -0300

    Busca de pacientes em uma api e carregada no console

--- introducao-javascript/js/buscar-paciente.js
+++ introducao-javascript/js/buscar-paciente.js
@@ -1,1 +1,18 @@
-console.log('buscaar os pacientes');
+var butaoAdicionar = document.querySelector('#buscar-pacientes');
+
+butaoAdicionar.addEventListener('click', function() {
+    console.log('Buscando pacientes...');
+
+    var xhr = new XMLHttpRequest();     //Responsável por fazer requisição http
+
+    //Configurando o endereço a ser feita a requisição
+    xhr.open('GET', 'http://api-pacientes.herokuapp.com/pacientes'); 
+
+    //Carrega a requisição e mostra a resposta
+    xhr.addEventListener('load', function() {
+        console.log(xhr.responseText);
+    })
+
+    //Envia a requisição
+    xhr.send();
+});
