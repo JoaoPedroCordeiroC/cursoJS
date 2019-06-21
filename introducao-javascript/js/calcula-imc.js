f62c6cd7f3b6c7cb0576adb9171c03128acae2a9
JoaoPedroCordeiroC <joao.u@hotmail.com>
2019-06-21 10:01:08 -0300

    renomeada  classe principal para calcula-imc

---
+++ introducao-javascript/js/calcula-imc.js
@@ -0,0 +1,42 @@
+var titulo = document.querySelector('.titulo');
+titulo.textContent = 'Aparecida Nutricionaista';
+
+var pacientes = document.querySelectorAll('.paciente');
+
+for(var i = 0; i < pacientes.length; i++) {
+    var paciente = pacientes[i];
+
+    var tdPeso = paciente.querySelector('.info-peso');
+    var peso = tdPeso.textContent;
+    
+    var tdAltura = paciente.querySelector('.info-altura');
+    var altura = tdAltura.textContent;
+    
+    var tdImc = paciente.querySelector(".info-imc");
+    
+    var pesoValido = true;
+    var alturaValido = true;
+    
+    if(peso <= 0 || peso >= 1000) {
+        console.log('Peso inválido!');
+        pesoValido =  false;
+        tdImc.textContent = 'Peso Inválido!';
+        paciente.classList.add('paciente-invalido');
+    }
+    
+    if(altura <= 0 || altura >= 3.00) {
+        console.log('Altura inválido!');
+        pesoValido =  false;
+        tdImc.textContent = 'Altura Inválida!';
+        paciente.classList.add('paciente-invalido');    //Chama stilo do CSS
+    }
+    
+    if (alturaValido && pesoValido) {
+        var imc = peso / (altura * altura);
+
+        //Setando valor calculado em JS para campo imc do cliente no HTML
+        tdImc.textContent = imc.toFixed(2);
+    }
+}
+
+
